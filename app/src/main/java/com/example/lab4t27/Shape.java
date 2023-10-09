6ad6cfa3531c3704b8a1d5f5d7503d8c69eeda21
Romanshu-Bedi <93629205+Romanshu-Bedi@users.noreply.github.com>
2023-10-08 23:27:41 -0600

    Update Shape.java

--- app/src/main/java/com/example/lab4t27/Shape.java
+++ app/src/main/java/com/example/lab4t27/Shape.java
@@ -3,9 +3,10 @@ package com.example.lab4t27;
 public abstract class Shape {
     private int x;
     private int y;
+   
     private String colour = "white";
 
-    private String color = "black";
+
 
 
     public Shape(){
